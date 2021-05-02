7d9f9112304498f2f1e303eab1c3832525eb678e
Tetiana Lavrenko <tet.lavrenko@gmail.com>
2021-05-02 22:07:52 +0200

    added isAuthenticated check and code splitting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
 // import { v4 as genId } from 'uuid';
 import { Switch, Route } from 'react-router-dom';
 import AppBar from './components/AppBar';
-import ContactsPage from './pages/Contacts';
-import LoginPage from './pages/Login';
-import RegisterPage from './pages/Register';
-import HomePage from './pages/Home';
+// import ContactsPage from './pages/Contacts';
+// import LoginPage from './pages/Login';
+// import RegisterPage from './pages/Register';
+// import HomePage from './pages/Home';
 import authOperations from './redux/auth/auth-operations';
 import { connect } from 'react-redux';
 import PrivateRoute from './components/AppBar/PrivateRoute';
 import PublicRoute from './components/AppBar/PublicRoute';
+
+const HomePage = lazy(() =>
+  import('./pages/Home' /*WebpackChunkName: HomePage */),
+);
+const LoginPage = lazy(() =>
+  import('./pages/Login' /*WebpackChunkName: LoginPage */),
+);
+const RegisterPage = lazy(() =>
+  import('./pages/Register' /*WebpackChunkName: RegisterPage */),
+);
+const ContactsPage = lazy(() =>
+  import('./pages/Contacts' /*WebpackChunkName: ContactsPage */),
+);
 
 class App extends Component {
   componentDidMount() {
@@ -21,18 +34,20 @@ class App extends Component {
     return (
       <>
         <AppBar />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <PublicRoute
-            exact
-            path="/register"
-            restricted
-            component={RegisterPage}
-          />
-          <PublicRoute exact path="/login" restricted component={LoginPage} />
-          <PrivateRoute exact path="/contacts" component={ContactsPage} />
-          <Route component={HomePage} />
-        </Switch>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <PublicRoute
+              exact
+              path="/register"
+              restricted
+              component={RegisterPage}
+            />
+            <PublicRoute exact path="/login" restricted component={LoginPage} />
+            <PrivateRoute exact path="/contacts" component={ContactsPage} />
+            <Route component={HomePage} />
+          </Switch>
+        </Suspense>
       </>
     );
   }
