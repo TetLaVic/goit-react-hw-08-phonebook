8903ec5cc58ef04cce1dc7dbdacb1dbeef14724b
Tetiana Lavrenko <tet.lavrenko@gmail.com>
2021-05-05 16:43:41 +0200

    Updates

---
+++ static/js/7.6a5397b7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[7],{115:function(e,o,t){e.exports={center:"HomePage_center__1VzX4",bgImage:"HomePage_bgImage__3r8jH"}},125:function(e,o,t){"use strict";t.r(o),t.d(o,"default",(function(){return r}));t(0);var n=t(115),c=t.n(n),a=t(2),r=function(){return Object(a.jsx)("div",{className:c.a.bgImage,children:Object(a.jsx)("h1",{className:c.a.center,children:"Welcome to Your Phonebook!"})})}}}]);
+//# sourceMappingURL=7.6a5397b7.chunk.js.map
